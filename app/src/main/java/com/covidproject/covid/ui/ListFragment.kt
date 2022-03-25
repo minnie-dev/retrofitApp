7f25a4e4fe1a47477828b5e580018f1f1c1d9908
minnie-dev <jung.dev98@gmail.com>
2022-03-25 17:07:53 +0900

    220325 MVVM 패턴 & 데이터 불러온 목록 RecyclerView

--- app/src/main/java/com/covidproject/covid/ui/ListFragment.kt
+++ app/src/main/java/com/covidproject/covid/ui/ListFragment.kt
@@ -5,18 +5,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.covidproject.covid.databinding.FragmentListBinding
 import com.covidproject.covid.model.data.Vaccine
+import com.covidproject.covid.ui.adapter.ListRecyclerAdapter
 import com.covidproject.covid.viewmodel.RetrofitViewModel
 
 class ListFragment : Fragment() {
     lateinit var binding : FragmentListBinding
     private val retrofitViewModel : RetrofitViewModel by viewModels()
     private var dataList: List<Vaccine>? = null
+    lateinit var adapter: ListRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,9 +30,18 @@ class ListFragment : Fragment() {
 
         retrofitViewModel.vaccineLiveData.observe(viewLifecycleOwner) {
             if (it.data != dataList) dataList = it.data
+
+            adapter = ListRecyclerAdapter(dataList!!)
+            binding.listRecycler.apply {
+                layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
+                adapter = adapter
+            }
             Log.d("TAG", it.toString())
         }
 
+
+
         return binding.root
     }
+
 }
--- app/src/main/java/com/covidproject/covid/ui/adapter/ListRecyclerAdapter.kt
+++ app/src/main/java/com/covidproject/covid/ui/adapter/ListRecyclerAdapter.kt
@@ -1,24 +1,33 @@
 package com.covidproject.covid.ui.adapter
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.covidproject.covid.databinding.ItemListBinding
+import com.covidproject.covid.model.data.Vaccine
 
-class ListRecyclerAdapter : RecyclerView.Adapter<ListRecyclerAdapter.ViewHolder>() {
+class ListRecyclerAdapter(private val dataList : List<Vaccine>) : RecyclerView.Adapter<ListRecyclerAdapter.ViewHolder>() {
+    lateinit var binding : ItemListBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        binding = ItemListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(dataList[position])
     }
+
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return dataList.size
     }
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    inner class ViewHolder(binding: ItemListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data : Vaccine){
+            binding.nameText.text = data.facilityName
+            binding.numberText.text = data.phoneNumber
+        }
     }
 }
 
